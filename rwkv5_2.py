f6e71b7b412fcde88b5366fc546ba45c5506b462
SmerkyG <SmerkyG@users.noreply.github.com>
2024-11-26 17:35:31 -0800

    added configs and inference fixes

--- rwkv5_2.py
+++ rwkv5_2.py
@@ -27,7 +27,7 @@ class RWKV(torch.nn.Module):
     def __init__(self, cfg:Config = Config()):
         super().__init__()
         self.cfg = cfg
-        self.embed = nn.Parameter(torch.ones(cfg.vocab_size, cfg.d_model) * 1e-4)
+        self.embed = nn.Parameter(torch.empty(cfg.vocab_size, cfg.d_model))
         self.embed_norm = nn.LayerNorm(cfg.d_model)
         self.layers = nn.ModuleList([Layer(cfg, layer_id) for layer_id in range(cfg.n_layers)])
         self.lm_head_norm = nn.LayerNorm(cfg.d_model)
@@ -74,25 +74,29 @@ class TimeMixer(nn.Module):
         super().__init__()
         self.cfg = cfg
 
-        self.prenorm = nn.LayerNorm(cfg.d_model)
+        d_model = cfg.d_model
+        d_head = d_model // cfg.n_heads
 
-        self.tokenshift_receptance = nn.Parameter(torch.empty(cfg.d_model))
-        self.receptance = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
-        self.tokenshift_key = nn.Parameter(torch.empty(cfg.d_model))
-        self.key = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
-        self.tokenshift_value = nn.Parameter(torch.empty(cfg.d_model))
-        self.value = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
-        self.tokenshift_gate = nn.Parameter(torch.empty(cfg.d_model))
-        self.gate = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
-        self.output = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
+        self.prenorm = nn.LayerNorm(d_model)
+
+        self.tokenshift_receptance = nn.Parameter(torch.empty(1, 1, d_model))
+        self.tokenshift_key = nn.Parameter(torch.empty(1, 1, d_model))
+        self.tokenshift_value = nn.Parameter(torch.empty(1, 1, d_model))
+        self.tokenshift_gate = nn.Parameter(torch.empty(1, 1, d_model))
+
+        self.receptance = nn.Linear(d_model, d_model, bias=False)
+        self.key = nn.Linear(d_model, d_model, bias=False)
+        self.value = nn.Linear(d_model, d_model, bias=False)
+        self.gate = nn.Linear(d_model, d_model, bias=False)
+        self.output = nn.Linear(d_model, d_model, bias=False)
 
         # per-channel boost for current embedding
-        self.bonus = nn.Parameter(torch.ones(cfg.n_heads, cfg.d_model//cfg.n_heads))
+        self.bonus = nn.Parameter(torch.ones(cfg.n_heads, d_head))
 
         # per-channel decay multipliers applied to kv_state at each timestep
-        self.decay = nn.Parameter(torch.ones(cfg.n_heads, cfg.d_model//cfg.n_heads))
+        self.decay = nn.Parameter(torch.ones(cfg.n_heads, d_head))
         
-        self.group_norm = nn.GroupNorm(cfg.n_heads, cfg.d_model, eps=64e-5)
+        self.group_norm = nn.GroupNorm(cfg.n_heads, d_model, eps=64e-5)
 
     @staticmethod
     def single_timestep(r, k, v, u, w, kv_state): 
@@ -102,25 +106,27 @@ class TimeMixer(nn.Module):
 
         # transform inputs from BHK and put everything in float format for higher precision
         r = r.float().view(B, H, 1, K)
-        w = w.float().view(B, H, K, 1)
+        w = w.float().view(1, H, K, 1)
         k = k.float().view(B, H, K, 1)
         v = v.float().view(B, H, 1, V)
-        u = u.unsqueeze(-1).float() # 1HK1
-        w = w.unsqueeze(-1).float() # 1HK1
+        u = u.float().view(1, H, K, 1)
+
+        kv = k @ v # BHK1 @ BH1V = BHKV
 
         # start with the existing kv state
         y = kv_state        # BHKV
         # apply the u boost to the current k @ v and add it to that
-        y = y + (k @ v) * u # BHKV * HK1 + BHKV = BHKV
+        y = y + kv * u # BHKV + BHKV * 1HK1 = BHKV
         # apply receptance to that whole result
         out = r @ y         # BH1K @ BHKV = BH1V
+        # remove an extra useless dimension from the output
+        out = out.squeeze(-2).to(original_dtype) # BHV
 
         # finally, decay the kv state and add in the latest k @ v
-        kv_state = kv_state * w         # BHKV
-        kv_state = kv_state + (k @ v)   # BHKV * HK1 + BHKV = BHKV
+        kv_state = kv_state * w    # BHKV * BHK1 = BHKV
+        kv_state = kv_state + kv   # BHKV + BHKV = BHKV
 
-        # remove an extra useless dimension from the output
-        return out.squeeze(-2), kv_state # BHV, BHKV
+        return out, kv_state # BHV, BHKV
 
     def forward(self, hidden_state_in : Tensor, x_state : Tensor, kv_state : Tensor): # x (B,T,C), x_state (B,C), kv_state (B,H,K,V)
         x = self.prenorm(hidden_state_in)
@@ -134,25 +140,23 @@ class TimeMixer(nn.Module):
         x_shifted_one_to_the_past = torch.cat((x_state.unsqueeze(-2), x[:,:-1]), dim=1)
 
         # token shift the incoming token embeddings for the receptance, key, value, and gate
-        x_r = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_receptance)
-        x_k = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_key)
-        x_v = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_value)
-        x_gate = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_gate)
+        x_receptance = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_receptance)
+        x_key = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_key)
+        x_value = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_value)
+        x_gate = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_gate)
                                            
         # the extra dimensions are being added here to enable matrix multiplications per timestep
-        r = self.receptance(x_r).view(B,T,H,1,K) # BTH1K
-        k = self.key(x_k).view(B,T,H,K,1) # BTHK1
-        v = self.value(x_v).view(B,T,H,1,K) # BTH1K
+        r = self.receptance(x_receptance).view(B,T,H,1,K) # BTH1K
+        k = self.key(x_key).view(B,T,H,K,1) # BTHK1
+        v = self.value(x_value).view(B,T,H,1,K) # BTH1K
         gate = self.gate(x_gate) # BTC
 
-        # adding an extra dimension for convenience in multiplication later
-        u = self.u.unsqueeze(-1) # HK1
         # this forces the decays to end up in the range 0...1 using a nicely differentiable function
-        w = torch.exp(-torch.exp(self.w.float())).unsqueeze(-1) # HK1
+        decay = torch.exp(-torch.exp(self.decay.float())) # HK
 
         out = torch.empty(B, T, H, K, dtype=x.dtype, device=x.device)
         for t in range(T):
-            out[:,t], kv_state = TimeMixer.single_timestep(r[:,t], k[:,t], v[:,t], u, w, kv_state)
+            out[:,t], kv_state = TimeMixer.single_timestep(r[:,t], k[:,t], v[:,t], self.bonus, decay, kv_state)
 
         # apply group normalization to each head and recombine the heads
         out = self.group_norm(out.view(B*T, C)).view(B, T, C) # BTC
@@ -161,7 +165,7 @@ class TimeMixer(nn.Module):
         out = out * nn.functional.silu(gate) # BTC
 
         # project the output
-        out = self.Wproj_out(out) # BTC
+        out = self.output(out) # BTC
 
         return hidden_state_in + out, x_state_out, kv_state
 
@@ -195,14 +199,14 @@ class ChannelMixer(nn.Module):
         # token shift is just a learned linear interpolation between the current and previous token embeddings in the sequence
         # this is done by lerping between x and the shifted x we just calculated
         # note that it is a per-channel learned interpolation amount, not just a single value per head
-        x_in = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_in)
-        x_gate = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_gate)
+        # PLEASE NOTE THAT THE DIRECTION OF THE LERP CHANGED IN RWKV-6
+        x_in = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_in)
+        x_gate = torch.lerp(x_shifted_one_to_the_past, x, self.tokenshift_gate)
 
-        # project to 3x larger hidden dimension
+        # project to 3.5x larger hidden dimension
         # this is 4x for vanilla transformers FFN, but it's typical to reduce it when adding new parameters 
         #  to allow comparison models with the same number of total parameters for a given d_model, n_layer
-        # in rwkv5's case, if you drop it down to 3x you end up making up for the extra parameters in the gate
-        # in rwkv6's case, if you drop it down to 3.5x you end up making up for the extra parameters in the gate
+        # if you drop it down to 3.5x you end up making up for the extra parameters in the gate
         hidden = self.W_in(x_in)
 
         # relu^2 activation function
@@ -221,3 +225,62 @@ class ChannelMixer(nn.Module):
 if __name__ == "__main__":
     model = RWKV()
     model.forward(torch.ones(1,2,dtype=torch.long))
+
+def convert_params_from_pth(model_params):   
+    # map the state dict entries to our naming convention
+
+    model_params['embed'] = model_params.pop('emb.weight')
+    model_params['embed_norm.weight'] = model_params.pop('blocks.0.ln0.weight')
+    model_params['embed_norm.bias'] = model_params.pop('blocks.0.ln0.bias')
+
+    model_params['lm_head_norm.weight'] = model_params.pop('ln_out.weight')
+    model_params['lm_head_norm.bias'] = model_params.pop('ln_out.bias')
+    model_params['lm_head_unembed.weight'] = model_params.pop('head.weight')
+
+    replacements = {
+        '.ln1.weight' : '.time_mixer.prenorm.weight',
+        '.ln1.bias' : '.time_mixer.prenorm.bias',
+
+        '.ln2.weight' : '.channel_mixer.prenorm.weight',
+        '.ln2.bias' : '.channel_mixer.prenorm.bias',
+
+        # time mixer token shift lerp amounts
+        '.att.time_mix_r' : '.time_mixer.tokenshift_receptance',
+        '.att.time_mix_w' : '.time_mixer.tokenshift_decay',
+        '.att.time_mix_k' : '.time_mixer.tokenshift_key',
+        '.att.time_mix_v' : '.time_mixer.tokenshift_value',
+        '.att.time_mix_g' : '.time_mixer.tokenshift_gate',
+
+        # bonus
+        '.time_faaaa' : '.bonus',
+
+        # decay
+        '.time_decay' : '.decay',
+
+        # group_norm
+        '.ln_x.weight' : '.group_norm.weight',
+        '.ln_x.bias' : '.group_norm.bias',
+
+        # channel mixer
+        '.ffn.time_mix_k' : '.channel_mixer.tokenshift_in',
+        '.ffn.time_mix_r' : '.channel_mixer.tokenshift_gate',
+        '.ffn.key.weight' : '.channel_mixer.W_in.weight',
+        '.ffn.value.weight' : '.channel_mixer.W_out.weight',
+        '.ffn.receptance.weight' : '.channel_mixer.gate.weight',
+    }
+
+    for k in list(model_params.keys()):
+        p = model_params.pop(k)
+
+        for needle, replacement in replacements.items():
+            if k.endswith(needle):
+                k = k.replace(needle, replacement)
+
+        k = k.replace('blocks.', 'layers.')
+
+        k = k.replace('.att.', '.time_mixer.')
+        k = k.replace('.ffn.', '.channel_mixer.')
+
+        model_params[k] = p
+
+    return model_params
--- rwkv6.py
+++ rwkv6.py
@@ -3,14 +3,14 @@ from torch import nn, Tensor
 from dataclasses import dataclass
 
 # config, layer state and channel mixer are unchanged from RWKV5
-from rwkv5_2 import Config, LayerState, ChannelMixer
+from rwkv5_2 import Config, LayerState
 
 # Only time mixer changes for RWKV6
 class RWKV(torch.nn.Module):
     def __init__(self, cfg:Config = Config()):
         super().__init__()
         self.cfg = cfg
-        self.embed = nn.Parameter(torch.ones(cfg.vocab_size, cfg.d_model) * 1e-4)
+        self.embed = nn.Parameter(torch.empty(cfg.vocab_size, cfg.d_model))
         self.embed_norm = nn.LayerNorm(cfg.d_model)
         self.layers = nn.ModuleList([Layer(cfg, layer_id) for layer_id in range(cfg.n_layers)])
         self.lm_head_norm = nn.LayerNorm(cfg.d_model)
@@ -99,6 +99,7 @@ class TimeMixer(nn.Module):
         self.cfg = cfg
 
         d_model = cfg.d_model
+        d_head = d_model // cfg.n_heads
 
         self.prenorm = nn.LayerNorm(d_model)
 
@@ -113,8 +114,13 @@ class TimeMixer(nn.Module):
             lora_ranks = lora_ranks_iter
 
         self.ddlerp_premix = nn.Parameter(torch.empty(1, 1, d_model))
-        self.ddlerps = nn.ModuleList([DDLerp(d_model, lora_ranks.ddlerp_lora) for _ in range(5)])
-        self.decay_lora = LoRA_MLP(d_model, lora_ranks.decay_lora)
+        self.ddlerp_decay = DDLerp(d_model, lora_ranks.ddlerp_lora)
+        self.ddlerp_key = DDLerp(d_model, lora_ranks.ddlerp_lora)
+        self.ddlerp_value = DDLerp(d_model, lora_ranks.ddlerp_lora)
+        self.ddlerp_receptance = DDLerp(d_model, lora_ranks.ddlerp_lora)
+        self.ddlerp_gate = DDLerp(d_model, lora_ranks.ddlerp_lora)
+
+        self.decay = LoRA_MLP(d_model, lora_ranks.decay_lora)
         self.receptance = nn.Linear(d_model, d_model, bias=False)
         self.key = nn.Linear(d_model, d_model, bias=False)
         self.value = nn.Linear(d_model, d_model, bias=False)
@@ -122,7 +128,7 @@ class TimeMixer(nn.Module):
         self.output = nn.Linear(d_model, d_model, bias=False)
 
         # per-channel boost for current embedding
-        self.bonus = nn.Parameter(torch.ones(cfg.n_heads, d_model//cfg.n_heads))
+        self.bonus = nn.Parameter(torch.ones(cfg.n_heads, d_head))
 
         self.group_norm = nn.GroupNorm(cfg.n_heads, d_model, eps=64e-5)
 
@@ -137,7 +143,7 @@ class TimeMixer(nn.Module):
         w = w.float().view(B, H, K, 1)
         k = k.float().view(B, H, K, 1)
         v = v.float().view(B, H, 1, V)
-        u = u.unsqueeze(-1).float() # 1HK1
+        u = u.float().view(1, H, K, 1)
 
         kv = k @ v # BHK1 @ BH1V = BHKV
 
@@ -154,7 +160,6 @@ class TimeMixer(nn.Module):
         kv_state = kv_state * w    # BHKV * BHK1 = BHKV
         kv_state = kv_state + kv   # BHKV + BHKV = BHKV
 
-        # remove an extra useless dimension from the output
         return out, kv_state # BHV, BHKV
 
     def forward(self, hidden_state_in : Tensor, x_state : Tensor, kv_state : Tensor): # x (B,T,C), x_state (B,C), kv_state (B,H,K,V)
@@ -170,16 +175,20 @@ class TimeMixer(nn.Module):
 
         # token shift the incoming token embeddings for the receptance, key, value, gate, and decay
         x_premixed = torch.lerp(x, x_shifted_one_to_the_past, self.ddlerp_premix)
-        x_decay, x_k, x_v, x_r, x_gate = [ddlerp(x_premixed, x, x_shifted_one_to_the_past) for ddlerp in self.ddlerps]
+        x_decay = self.ddlerp_decay(x_premixed, x, x_shifted_one_to_the_past)
+        x_key = self.ddlerp_key(x_premixed, x, x_shifted_one_to_the_past)
+        x_value = self.ddlerp_value(x_premixed, x, x_shifted_one_to_the_past)
+        x_receptance = self.ddlerp_receptance(x_premixed, x, x_shifted_one_to_the_past)
+        x_gate = self.ddlerp_gate(x_premixed, x, x_shifted_one_to_the_past)
                                            
         # project and separate out our vectors into attention heads
-        r = self.receptance(x_r).view(B,T,H,K) # BTHK
-        k = self.key(x_k).view(B,T,H,K) # BTHK
-        v = self.value(x_v).view(B,T,H,K) # BTHK
+        r = self.receptance(x_receptance).view(B,T,H,K) # BTHK
+        k = self.key(x_key).view(B,T,H,K) # BTHK
+        v = self.value(x_value).view(B,T,H,K) # BTHK
         gate = self.gate(x_gate) # BTC
 
         # per-channel data-dependent decays generated inexpensively via low rank adaptation
-        log_neglog_of_decay = self.decay_lora(x_decay)
+        log_neglog_of_decay = self.decay(x_decay)
         # separate out into attention heads
         log_neglog_of_decay = log_neglog_of_decay.view(B,T,H,K)
         # this forces the decays to end up in the range 0...1 using a nicely differentiable function
@@ -188,7 +197,7 @@ class TimeMixer(nn.Module):
 
         out = torch.empty(B, T, H, K, dtype=x.dtype, device=x.device)
         for t in range(T):
-            out[:,t], kv_state = TimeMixer.single_timestep(r[:,t], k[:,t], v[:,t], self.bonus.view(1,H,K), decay[:,t], kv_state)
+            out[:,t], kv_state = TimeMixer.single_timestep(r[:,t], k[:,t], v[:,t], self.bonus, decay[:,t], kv_state)
 
         # apply group normalization to each head and recombine the heads
         out = self.group_norm(out.view(B*T, C)).view(B, T, C) # BTC
@@ -201,6 +210,58 @@ class TimeMixer(nn.Module):
 
         return hidden_state_in + out, x_state_out, kv_state
 
+class ChannelMixer(nn.Module):
+    def __init__(self, cfg:Config, layer_id:int):
+        super().__init__()
+        self.cfg = cfg
+        self.prenorm = nn.LayerNorm(cfg.d_model)
+        self.tokenshift_in = nn.Parameter(torch.empty(1, 1, cfg.d_model))
+        self.tokenshift_gate = nn.Parameter(torch.empty(1, 1, cfg.d_model))
+        d_ffn = cfg.d_ffn or int(cfg.d_model * 3.5)//32*32
+        self.W_in = nn.Linear(cfg.d_model, d_ffn, bias=False)
+        self.W_out = nn.Linear(d_ffn, cfg.d_model, bias=False)
+        self.gate = nn.Linear(cfg.d_model, cfg.d_model, bias=False)
+
+    def forward(self, hidden_state_in : Tensor, x_state : Tensor): # x (B,T,C), x_state (B,C)
+        x = self.prenorm(hidden_state_in)
+        x_state_out = x[:, -1]
+
+        # token shift the incoming token embeddings for both the input projection and gate
+
+        # token shift is like a a very efficient 1D convolution with kernel size 2, similar to undilated causal conv in WaveNet
+        # this gives each head the ability to choose which parts of the time-series to pay attention to
+        # it acts like a vertical forget gate between layers, choosing which parts of the recent past to accrue and which to ignore
+
+        # we want the token embeddings shifted over by one towards the past
+        # to get this, we take the last token embedding processed and append all but one of the current token embeddings to it
+        # (the last token embedding processed is what's stored in the x_state)
+        x_shifted_one_to_the_past = torch.cat((x_state.unsqueeze(-2), x[:,:-1]), dim=1)
+
+        # token shift is just a learned linear interpolation between the current and previous token embeddings in the sequence
+        # this is done by lerping between x and the shifted x we just calculated
+        # note that it is a per-channel learned interpolation amount, not just a single value per head
+        # PLEASE NOTE THAT THE DIRECTION OF THE LERP CHANGED IN RWKV-6
+        x_in = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_in)
+        x_gate = torch.lerp(x, x_shifted_one_to_the_past, self.tokenshift_gate)
+
+        # project to 3.5x larger hidden dimension
+        # this is 4x for vanilla transformers FFN, but it's typical to reduce it when adding new parameters 
+        #  to allow comparison models with the same number of total parameters for a given d_model, n_layer
+        # if you drop it down to 3.5x you end up making up for the extra parameters in the gate
+        hidden = self.W_in(x_in)
+
+        # relu^2 activation function
+        hidden = torch.square(torch.relu(hidden))
+
+        # project back out to d_model
+        out = self.W_out(hidden)
+
+        # apply sigmoid gate
+        gate = self.gate(x_gate)
+        out = out * torch.sigmoid(gate)
+
+        return hidden_state_in + out, x_state_out
+    
 if __name__ == "__main__":
     model = RWKV()
     model.forward(torch.ones(1,2,dtype=torch.long))
@@ -223,21 +284,21 @@ def convert_params_from_pth(model_params):
         '.ln2.weight' : '.channel_mixer.prenorm.weight',
         '.ln2.bias' : '.channel_mixer.prenorm.bias',
 
-        # time mixer token shift lerps
+        # time mixer token shift lerp bases
         '.att.time_maa_x' : '.time_mixer.ddlerp_premix',
-        '.att.time_maa_r' : '.time_mixer.ddlerps.0.lora.base',
-        '.att.time_maa_w' : '.time_mixer.ddlerps.1.lora.base',
-        '.att.time_maa_k' : '.time_mixer.ddlerps.2.lora.base',
-        '.att.time_maa_v' : '.time_mixer.ddlerps.3.lora.base',
-        '.att.time_maa_g' : '.time_mixer.ddlerps.4.lora.base',
+        '.att.time_maa_r' : '.time_mixer.ddlerp_receptance.lora.base',
+        '.att.time_maa_w' : '.time_mixer.ddlerp_decay.lora.base',
+        '.att.time_maa_k' : '.time_mixer.ddlerp_key.lora.base',
+        '.att.time_maa_v' : '.time_mixer.ddlerp_value.lora.base',
+        '.att.time_maa_g' : '.time_mixer.ddlerp_gate.lora.base',
 
         # bonus
         '.time_faaaa' : '.bonus',
 
         # decay_lora
-        '.time_decay' : '.decay_lora.base',
-        '.time_decay_w1' : '.decay_lora.W_a',
-        '.time_decay_w2' : '.decay_lora.W_b',
+        '.time_decay' : '.decay.base',
+        '.time_decay_w1' : '.decay.W_a',
+        '.time_decay_w2' : '.decay.W_b',
 
         # group_norm
         '.ln_x.weight' : '.group_norm.weight',
@@ -263,14 +324,15 @@ def convert_params_from_pth(model_params):
         k = k.replace('.att.', '.time_mixer.')
         k = k.replace('.ffn.', '.channel_mixer.')
 
+        maa_names = ['decay', 'key', 'value', 'receptance', 'gate']
         if k.endswith('.time_mixer.time_maa_w1'):
             for i, w in enumerate(p.chunk(5, dim=1)):
-                model_params[k.replace('.time_maa_w1', f'.ddlerps.{i}.lora.W_a')] = w
+                model_params[k.replace('.time_maa_w1', f'.ddlerp_{maa_names[i]}.lora.W_a')] = w
             continue
 
         if k.endswith('.time_mixer.time_maa_w2'):
             for i, w in enumerate(p.chunk(5, dim=0)):
-                model_params[k.replace('.time_maa_w2', f'.ddlerps.{i}.lora.W_b')] = w.squeeze(0)
+                model_params[k.replace('.time_maa_w2', f'.ddlerp_{maa_names[i]}.lora.W_b')] = w.squeeze(0)
             continue
 
         model_params[k] = p
